b8bf288521c94cbfd71bccb3f4f75f6d5dd6da1c
Lhavaa <battsagaanlhagvaerdene025@gmail.com>
2024-04-09 14:36:10 +0800

    adfasd

---
+++ lib/models/coffee_shop.dart
@@ -0,0 +1,55 @@
+import 'package:coffeetute/models/coffee.dart';
+import 'package:flutter/material.dart';
+
+
+class CoffeeShop extends ChangeNotifier{
+  //coffee for sale list
+  final List<Coffee> _shop = [
+    //black coffee
+    Coffee(
+      name: 'Long Black',
+      price: "7500",
+      imagePath: "lib/images/black.png",
+    ),
+    //latte
+    Coffee(
+      name: 'Latte',
+      price: "7000",
+      imagePath: "lib/images/latte.png",
+    ),
+    //iced coffee
+    Coffee(
+      name: 'Iced Coffee',
+      price: "8500",
+      imagePath: "lib/images/iced_coffee.png",
+    ),
+    //espresso
+    Coffee(
+      name: 'Espresso',
+      price: "7500",
+      imagePath: "lib/images/espresso.png",
+    ),
+  ];
+
+  //user cart
+  List<Coffee> _userCart = [];
+
+  //get coffee list
+  List<Coffee> get coffeeShop => _shop;
+
+  //get user cart
+  List<Coffee> get userCart => _userCart;
+
+  //add item to cart
+  void addItemToCart(Coffee coffee){
+    _userCart.add(coffee);
+    notifyListeners();
+  }
+
+  //remove item from cart
+  void removeItemFromCart(Coffee coffee){
+    _userCart.remove(coffee);
+    notifyListeners();
+  }
+
+}
---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,84 @@
+import 'package:coffeetute/components/coffee_tile.dart';
+import 'package:coffeetute/models/coffee.dart';
+import 'package:coffeetute/models/coffee_shop.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+
+void removeFromCart(Coffee coffee){
+  Provider.of<CoffeeShop>(context, listen: false).removeItemFromCart(coffee);
+}
+
+// pay button tapped
+void payNow(){
+  /*
+
+  */
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child) => SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+
+            //heading
+            Text('Your Cart',
+            style: TextStyle(fontSize: 20),
+            ),
+
+            //list of cart item
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.userCart.length,
+                itemBuilder:  (context, index) {
+              //get individual cart items
+              Coffee eachCoffee = value.userCart[index];
+
+              //return coffee tile
+              return CoffeeTile(
+                coffee: eachCoffee, 
+                onPressed: () => removeFromCart(eachCoffee), 
+                icon: Icon(Icons.delete),
+                );
+            },
+            ),
+          ),
+          //pay button
+          GestureDetector(
+            onTap: payNow,
+            child: Container(
+              padding: const EdgeInsets.all(25),
+              width: double.infinity,
+              decoration: BoxDecoration(
+              color: Colors.brown,
+              borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Center(
+                child: Text(
+                  "Pay now", 
+                  style: TextStyle(color: Colors.white),
+                          ),
+              ),
+            
+            ),
+          )
+        ],
+       ),
+      ),
+    ),
+    );
+  }
+}
